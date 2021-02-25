9b8d5c8ada4e6ba3442bc93e93b863b9d596e1a0
Andres <agaviriat@gmail.com>
2021-02-24 20:49:18 -0500

    1

--- models/name.py
+++ models/name.py
@@ -5,11 +5,11 @@ from odoo import api, models, fields
 class FirstNameLastName(models.Model):
     _inherit = 'res.partner'
 
+    sec = fields.Char(string='Sector')
+    tamemp = fields.Char(string='Tamaño empresa')
+
 class  modelo76inf(models.Models):
     _description  =  'Modelo para Manipular Many2many'
     _name  =  'modelo.many2many'
-    name = fields.Char('como')
 
-    sec = fields.Char(string='Sector')
-    tamemp = fields.Char(string='Tamaño empresa')
     como = fields.Many2many('model.many2many', string="Linea de producto")
