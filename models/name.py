dad4322fad425fa1a0c502e15d6253fd183dc2e3
Andres <agaviriat@gmail.com>
2021-02-25 20:05:16 -0500

    2

--- models/name.py
+++ models/name.py
@@ -5,29 +5,6 @@ from odoo import api, models, fields
 class FirstNameLastName(models.Model):
     _inherit = 'res.partner'
 
-    
-    sec = fields.Selection(
-        [
-            ('1', 'Almacenes por Departamento'),
-            ('2', 'Belleza y Cosmeticas'),
-
-        ], "Sector",
-    )
-    com = fields.Selection(
-        [
-            ('1', 'FaceBook'),
-            ('2', 'Acesor Comercial'),
-
-        ], "Sector",
-    )
-    tipo = fields.Selection(
-        [
-            ('1', 'Actual'),
-            ('2', 'Prospecto'),
-            ('3', 'Otro'),
-
-        ], "Sector",
-    )
     tamemp = fields.Char(string='Tamaño empresa')
     linea = fields.Many2many('model.manipulate.many2many', string="Linea de producto")
 
