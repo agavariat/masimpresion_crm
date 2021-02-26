8f1d840ba3feec5a75a84a448d38d584ff154bd5
Andres <agaviriat@gmail.com>
2021-02-25 20:21:28 -0500

    1

--- models/name.py
+++ models/name.py
@@ -35,23 +35,3 @@ class SeveralFields(models.Model):
 
     name = fields.Char('linea1')
     field_sec = fields.Selection([('almacenes_por_departamento', 'Almacenes por Departamento'),('belleza_cosmeticas', 'Belleza y Cosmeticas')])
-    sec = fields.Selection(
-        [
-            ('1', 'Almacenes por Departamento'),
-            ('2', 'Belleza y Cosmeticas'),
-
-        ], "Sector",
-    )
-    com = fields.Selection(
-        [
-            ('1', 'FaceBook'),
-            ('2', 'Acesor Comercial'),
-        ], "Sector",
-    )
-    tip = fields.Selection(
-        [
-            ('1', 'Actual'),
-            ('2', 'Prospecto'),
-            ('3', 'Otro'),
-        ], "Sector",
-    )
