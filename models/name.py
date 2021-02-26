0a7d1cfe7c8c1e51898e1f7d4d5ffeb196a01803
Andres <agaviriat@gmail.com>
2021-02-25 20:38:27 -0500

    1

--- models/name.py
+++ models/name.py
@@ -7,14 +7,14 @@ class FirstNameLastName(models.Model):
 
     tamemp = fields.Char(string='Tamaño empresa')
     linea = fields.Many2many('model.manipulate.many2many', string="Linea de producto")
+    Clisse = fields.Selection(
+        [('50', 'Si'),
+         ('51', 'No'),
+        ], "Clisse",
+    )
 
 class SeveralFields(models.Model):
     _description = 'Modelo para Manipular Many2many'
     _name = 'model.manipulate.many2many'
 
     name = fields.Char('linea1')
-
-class SelectionsFields(models.Model)
-    _inherit = 'res.partner'
-
-    field_sec = fields.Selection([('almacenes_por_departamento', 'Almacenes por Departamento'),('belleza_cosmeticas', 'Belleza y Cosmeticas')])
