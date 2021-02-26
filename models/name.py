c77b8be179d9893df11ac13963f2c5d4f57d420c
Andres <agaviriat@gmail.com>
2021-02-25 22:36:29 -0500

    1

--- models/name.py
+++ models/name.py
@@ -4,6 +4,14 @@ from odoo import api, models, fields
 
 class FirstNameLastName(models.Model):
     _inherit = 'res.partner'
+class SeveralFields(models.Model):
+    _description = 'Modelo para Manipular Many2many'
+    _name = 'model.manipulate.many2many'
+    name = fields.Char('Linea de producto')
+
+class SelectionField(models.Model):
+    _inherit = 'res.partner'
+    _name = 'select'
 
     tamemp = fields.Selection(
             [
@@ -14,14 +22,6 @@ class FirstNameLastName(models.Model):
             ], "Tamaño de Empresa",
         )
     linea = fields.Many2many('model.manipulate.many2many', string="Linea de producto")
-    sec1 = fields.Selection(
-            [
-                ('1', 'Almacenes por Departamento'),
-                ('2', 'Belleza y Cosmeticas'),
-                ('3', 'Alimentos'),
-                ('4', 'Bebidas'),
-            ], "Sector",
-        )
     sec = fields.Selection(
             [
                 ('1', 'Almacenes por Departamento'),
@@ -31,14 +31,3 @@ class FirstNameLastName(models.Model):
             ], "Sector",
         )
 
-class SeveralFields(models.Model):
-    _description = 'Modelo para Manipular Many2many'
-    _name = 'model.manipulate.many2many'
-    name = fields.Char('Linea de producto')
-
-class SelectionField(models.Model):
-    _inherit = 'res.partner'
-    _name = 'select'
-class SelectionField(models.Model):
-    _inherit = 'res.partner'
-    _name = 'select1'
