8f3d831e8f7bb834bf86cb83cb9960df96eb6713
Andres <agaviriat@gmail.com>
2021-02-26 08:15:08 -0500

    1

--- models/name.py
+++ models/name.py
@@ -7,22 +7,30 @@ class FirstNameLastName(models.Model):
     _inherit = 'res.partner'
 
     tamemp = fields.Selection(
-            [
-                ('1', 'Grande'),
-                ('2', 'Mediana'),
-                ('3', 'Pequeña'),
-                ('4', 'Empresa Personal'),
-            ], "Tamaño de Empresa",
-        )
+        [
+            ('1', 'Grande'),
+            ('2', 'Mediana'),
+            ('3', 'Pequeña'),
+            ('4', 'Empresa Personal'),
+        ], "Tamaño de Empresa",
+    )
     linea = fields.Many2many('model.manipulate.many2many', string="Linea de producto")
     sec = fields.Selection(
-            [
-                ('1', 'Almacenes por Departamento'),
-                ('2', 'Belleza y Cosmeticas'),
-                ('3', 'Alimentos'),
-                ('4', 'Bebidas'),
-            ], "Sector",
-        )
+        [
+            ('1', 'Almacenes por Departamento'),
+            ('2', 'Belleza y Cosmeticas'),
+            ('3', 'Alimentos'),
+            ('4', 'Bebidas'),
+        ], "Sector",
+    )
+    client = = fields.Selection(
+        [
+            ('1', 'Almacenes por Departamento'),
+            ('2', 'Belleza y Cosmeticas'),
+            ('3', 'Alimentos'),
+            ('4', 'Bebidas'),
+        ], "Sector",
+    )
 class SeveralFields(models.Model):
     _description = 'Modelo para Manipular Many2many'
     _name = 'model.manipulate.many2many'
