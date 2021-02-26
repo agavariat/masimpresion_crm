833a8e7db0a029cc8012e51befb77b034391fe16
Andres <agaviriat@gmail.com>
2021-02-25 20:23:16 -0500

    1

--- models/name.py
+++ models/name.py
@@ -5,29 +5,9 @@ from odoo import api, models, fields
 class FirstNameLastName(models.Model):
     _inherit = 'res.partner'
 
-    
-    sec = fields.Selection(
-        [
-            ('1', 'Almacenes por Departamento'),
-            ('2', 'Belleza y Cosmeticas'),
-
-        ], "Sector",
-    )
-    com = fields.Selection(
-        [
-            ('1', 'FaceBook'),
-            ('2', 'Acesor Comercial'),
-        ], "Sector",
-    )
-    tip = fields.Selection(
-        [
-            ('1', 'Actual'),
-            ('2', 'Prospecto'),
-            ('3', 'Otro'),
-        ], "Sector",
-    )
     tamemp = fields.Char(string='Tamaño empresa')
     linea = fields.Many2many('model.manipulate.many2many', string="Linea de producto")
+     field_sec = fields.Selection([('almacenes_por_departamento', 'Almacenes por Departamento'),('belleza_cosmeticas', 'Belleza y Cosmeticas')])
 
 class SeveralFields(models.Model):
     _description = 'Modelo para Manipular Many2many'
